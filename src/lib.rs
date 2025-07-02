c5cc4a2a5512eaaa0b5f762e8fcd3c2a4bb72cc4
0xgleb <gleb.dianov@protonmail.com>
2025-07-02 17:17:51 +0400

    add bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use alloy::sol;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -5,3 +6,8 @@ pub struct Env {
     #[clap(short, long, env)]
     pub ws_rpc_url: String,
 }
+
+sol!(
+    #![sol(all_derives = true, rpc)]
+    IOrderBookV4, "lib/rain.orderbook.interface/out/IOrderBookV4.sol/IOrderBookV4.json"
+);
