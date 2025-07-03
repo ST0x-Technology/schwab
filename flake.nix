ecda0a117c56a887c9b5ad2e6b4e9a5eab083171
0xgleb <gleb.dianov@protonmail.com>
2025-07-03 17:46:12 +0400

    build IERC20

--- flake.nix
+++ flake.nix
@@ -10,7 +10,19 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = rainix.pkgs.${system};
       in {
-        packages = rainix.packages.${system};
+        packages = let rainixPkgs = rainix.packages.${system};
+        in rainixPkgs // {
+          prepSolArtifacts = rainix.mkTask.${system} {
+            name = "prep-sol-artifacts";
+            additionalBuildInputs = rainix.sol-build-inputs.${system};
+            body = ''
+              set -euxo pipefail
+              (cd lib/rain.orderbook.interface/ && rainix-sol-prelude)
+              (cd lib/forge-std/ && rainix-sol-prelude)
+            '';
+          };
+        };
+
         devShell = pkgs.mkShell {
           shellHook = rainix.devShells.${system}.default.shellHook;
           buildInputs = [ pkgs.sqlx-cli ]
