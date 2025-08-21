fe072b156b51224eb689716cea29f87f19c41e5b
0xgleb <gleb.dianov@protonmail.com>
2025-08-22 02:03:10 +0700

    launch fn & server bin

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use rain_schwab::env::{Env, setup_tracing};
-use rain_schwab::run;
+use rain_schwab::launch;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -8,6 +8,6 @@ async fn main() -> anyhow::Result<()> {
     let env = Env::try_parse()?;
     setup_tracing(&env.log_level);
 
-    run(env).await?;
+    launch(env).await?;
     Ok(())
 }
