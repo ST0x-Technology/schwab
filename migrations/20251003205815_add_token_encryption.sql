2f01e40239fefe4352e1af49a7bd6a57b78ea4b2
0xgleb <gleb.dianov@protonmail.com>
2025-10-07 15:01:55 -0300

    fixes

--- migrations/20251003205815_add_token_encryption.sql
+++ migrations/20251003205815_add_token_encryption.sql
@@ -2,7 +2,7 @@
 -- Version 1 = AES-256-GCM encrypted tokens
 ALTER TABLE schwab_auth
 ADD COLUMN encryption_version INTEGER NOT NULL DEFAULT 1
-CHECK (encryption_version = 1);
+CHECK (encryption_version > 0);
 
 -- Clear existing plaintext tokens (forces re-authentication after upgrade)
 -- This is a hard switch to encrypted tokens with no backwards compatibility
